c42b38ad4c2bbcee89738000709711fa1cc2d164
Valentin Hilbig <webmaster@scylla-charybdis.com>
2011-03-29 21:01:16 +0000

    Working version with stdin support

--- sendsock.py
+++ sendsock.py
@@ -5,9 +5,11 @@
 # Send cmd to socket
 #
 # $Log$
-# Revision 1.1  2011/01/21 16:23:02  tino
-# Just a quick and dirty thing to send commands to the rfbimg.py .sock
+# Revision 1.2  2011/03/29 21:01:16  tino
+# Working version with stdin support
 #
+# Revision 1.1  2011-01-21 16:23:02  tino
+# Just a quick and dirty thing to send commands to the rfbimg.py .sock
 
 import sys
 import os
@@ -19,10 +21,13 @@ def unixsocket(name):
 	return sock
 
 def get(err):
+	sock.flush()
 	while True:
-		s = sock.recv(4096)
+		s = sock.readline()
 		if not s and err!=0:
-			print "err EOF"
+			print "EOF"
+			sys.exit(err)
+		if s=="ko\n":
 			sys.exit(err)
 		if s=="ok\n" or not s:
 			return
@@ -30,13 +35,17 @@ def get(err):
 
 sock = None
 def send(s):
-	sock.sendall("%s\n" % s)
+	sock.write("%s\n" % s)
 	get(1)
 
 if __name__=='__main__':
-	sock = unixsocket(".sock")
-	for arg in sys.argv[1:]:
-		send(arg)
+	sock = unixsocket(".sock").makefile()
+	if len(sys.argv)==2 and sys.argv[1]=='-':
+		for arg in sys.stdin.readlines():
+			send(arg.strip())
+	else:
+		for arg in sys.argv[1:]:
+			send(arg)
 	send("exit")
 	get(0)
 
