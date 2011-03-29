88449e5aa8e44b9a530c82a6d0aa8600e9371f5f
Valentin Hilbig <webmaster@scylla-charybdis.com>
2011-03-29 21:02:35 +0000

    Generic version independent of scripts

--- todo.sh
+++ todo.sh
@@ -2,100 +2,70 @@
 # $Header$
 #
 # $Log$
-# Revision 1.3  2011/03/23 09:59:06  tino
+# Revision 1.4  2011/03/29 21:02:35  tino
+# Generic version independent of scripts
+#
+# Revision 1.3  2011-03-23 09:59:06  tino
 # much better but not independent of the target currently
 #
 # Revision 1.2  2011-03-16 20:18:20  tino
 # Added parameter support
-#
 
-STDWAIT=12
+set_exit()
+{
+atexit="$*"
+}
 
 exe()
 {
-(
 . script/X
-script="$1"
-shift
-. "$script"
-) >/dev/null
+. "script/$script"
 }
 
 d()
 {
+[ -z "$1" ] && return
 echo -n " .."
-case "$2" in
-save)	j="$c"; return;;
--)	c=; return;;
-esac
 
-keypressed "${1:-$STDWAIT}000" && exit
+snapshot=
+echo -n " $1"
 
-if [ -n "$j" ]
+script="$1"
+exe $2
+
+if [ -n "$snapshot" ]
 then
-	cp test.jpg "c/c$j.jpg"
-	echo -n " c$j.jpg"
-	j=''
+	cp test.jpg "c/$snapshot.jpg"
+	echo -n " $snapshot.jpg"
 fi
-[ -z "$2" ] && return
-
-have=:
-echo -n " $2"
-exe "script/$2" $3
-
-j=
-case "$2" in
-city)	j="$c";;
-bau)	j="b$c";;
-dom)	c=1; city="$2";;
-tank)	c=2; city="$2";;
-spat)	c=3; city="$2";;
-gral)	c=4; city="$2";;
-bust)	c=5; city="$2";;
-wanne)	c=6; city="$2";;
-bank)	c=7; city="$2";;
-mimo)	c=8; city="$2";;
-L)	j=9;;
-mil)	j=a;;
-sci)	j=b;;
-diplo)	j=c;;
-info)	j=d;;
-esac
 }
 
 run()
 {
-read -ru3 city || return
-
-j=
-c=
-
-d 1 "$city"
-if [ -n "$c" ]
-then
-	d 5 city
-	d 5 "$city"
-	d 5 city
-fi
-
-have=false
+atexit=""
 while	read -ru3 cmd args
 do
-	d '' "$cmd" "$args"
+	d "$cmd" "$args"
 done
 
-[ -n "$c" ] && $have && d '' city
-[ -n "$j" ] && d
+[ -n "$atexit" ] && d $atexit
 }
 
-tim=0
+d restart
+
+tim=1
 while
 	if	read -rt$tim cmd args
 	then
 		[ -n "$cmd" ] || exit
-		d 0 "$cmd" "$args"
+		case "$cmd" in
+		\!*)	cmd="${cmd:1}"
+			set -x;;
+		esac
+		d "$cmd" "$args"
+		set +x
 		echo
-		tim=0
+		tim=1
 		continue
 	fi
 
@@ -117,11 +87,9 @@ do
 	last="$next"
 	expr ":$next" \< ":$now" >/dev/null || continue
 
-	exec 3<"todo/$next"
 	echo -n "RUN"
-	run
+	run 3<"todo/$next"
 	echo -n " "
 	rm -vf "todo/$next"
-	exec 3<&-
 done
 
