d696767ca9bf6573807ef018163beaa9b1ac9d54
Valentin Hilbig <webmaster@scylla-charybdis.com>
2011-04-24 22:36:31 +0000

    saner run (less warnings)

--- click.php
+++ click.php
@@ -6,7 +6,10 @@
 # So protect it with BasicAuth and HTTPS, you have been warned.
 #
 # $Log$
-# Revision 1.3  2011/03/23 10:01:03  tino
+# Revision 1.4  2011/04/24 22:36:31  tino
+# saner run (less warnings)
+#
+# Revision 1.3  2011-03-23 10:01:03  tino
 # Mouse button select
 #
 # Revision 1.2  2010-11-16 08:08:51  tino
@@ -20,12 +23,12 @@ $fd = fsockopen("unix://.sock");
 socket_set_blocking($fd,0);
 $o="";
 $s="";
-if ($_GET["c"]!="")
+if (isset($_GET["c"]))
   {
     $o = sprintf("code %d", $_GET["c"]);
     $s = sprintf("code 0x%04x\n", $_GET["c"]);
   }
-elseif ($_GET["t"]!="")
+elseif (isset($_GET["t"]))
   {
     $a = explode("\n",$_GET["t"]);
 #    $s = "key ".$a[count($a)-1];
@@ -35,7 +38,7 @@ elseif ($_GET["t"]!="")
         $s = "code 10\n$s";
     }
   }
-elseif ($_GET["x"])
+elseif (isset($_GET["x"]))
   {
     $s = sprintf("mouse %d %d", $_GET["x"], $_GET["y"]);
     if (isset($_GET["b"]))
@@ -47,7 +50,7 @@ elseif ($_GET["x"])
           $o = sprintf("%s\n%s %d\n%s", $s, $s, $b, $s);
       }
   }
-elseif ($_GET["l"])
+elseif (isset($_GET["l"]))
   {
     $a = explode("\n",$_GET["l"]);
     $s = "learn ".$a[0];
