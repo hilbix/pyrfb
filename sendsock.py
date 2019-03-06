976b760bd078df2cb0bbea19559588295b0dba63
Valentin Hilbig <webmaster@scylla-charybdis.com>
2019-03-06 03:58:24 +0100

    sendsock.py modified

--- sendsock.py
+++ sendsock.py
@@ -1,21 +1,11 @@
 #!/usr/bin/env python
 #
-# $Header$
-#
 # Send cmd to socket
+# ./sendsock.py socketnr
+# ./sendsock.py socketnr lines..
 #
 # This Works is placed under the terms of the Copyright Less License,
 # see file COPYRIGHT.CLL.  USE AT OWN RISK, ABSOLUTELY NO WARRANTY.
-#
-# $Log$
-# Revision 1.3  2011/08/07 18:24:36  tino
-# CLL
-#
-# Revision 1.2  2011-03-29 21:01:16  tino
-# Working version with stdin support
-#
-# Revision 1.1  2011-01-21 16:23:02  tino
-# Just a quick and dirty thing to send commands to the rfbimg.py .sock
 
 import sys
 import os
@@ -45,12 +35,12 @@ def send(s):
 	get(1)
 
 if __name__=='__main__':
-	sock = unixsocket(".sock").makefile()
-	if len(sys.argv)==2 and sys.argv[1]=='-':
+	sock = unixsocket('sub/'+sys.argv[1]+'/sock').makefile()
+	if len(sys.argv)==2:
 		for arg in sys.stdin.readlines():
 			send(arg.strip())
 	else:
-		for arg in sys.argv[1:]:
+		for arg in sys.argv[2:]:
 			send(arg)
 	send("exit")
 	get(0)
