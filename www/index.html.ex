be32c5d4a678ed7a9d4133232299e90910c53fb4
Valentin Hilbig <webmaster@scylla-charybdis.com>
2019-04-18 17:44:14 +0200

    www/index.html.ex added

---
+++ www/index.html.ex
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Select VNC</title>
+<meta charset="utf-8"/>
+</head>
+<body>
+<ul>
+<li><a href="auto/?1">auto</a> <a href="vncs.html?1">VNC 1</a></li>
+<li><a href="auto/?2">auto</a> <a href="vncs.html?2">VNC 2</a></li>
+<li><a href="auto/?3">auto</a> <a href="vncs.html?3">VNC 3</a></li>
+<li><a href="auto/?4">auto</a> <a href="vncs.html?4">VNC 4</a></li>
+</ul>
+</body>
+</html>
+
