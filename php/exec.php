9e1bffbe92bbfcbdd0b98865c9fb3b4204c271bf
Valentin Hilbig <valentin.hilbig@sol-venetus.de>
2019-08-18 19:31:42 +0200

    php/exec.php modified

--- php/exec.php
+++ php/exec.php
@@ -4,17 +4,48 @@
 # This Works is placed under the terms of the Copyright Less License,
 # see file COPYRIGHT.CLL.  USE AT OWN RISK, ABSOLUTELY NO WARRANTY.
 
-header("Content-type: text/plain");
-header("Expires: -1");
-header("Pragma: no-cache");
+header('Content-type: text/plain');
+header('Expires: -1');
+header('Pragma: no-cache');
 
-$targ = intval(substr($_SERVER["PATH_INFO"],1));
-$root = $_SERVER["DOCUMENT_ROOT"];
-$script = $_SERVER["SCRIPT_NAME"];	// this must be relative to document root
+$targ = intval(substr($_SERVER['PATH_INFO'],1));
+$root = $_SERVER['DOCUMENT_ROOT'];
+$script = $_SERVER['SCRIPT_NAME'];	// this must be relative to document root
 $base = "$root/".dirname($script)."/$targ";
 if (!is_dir($base)) die("wrong $targ");
 
 $dirs = [ 'dir'=>'e', 'learn'=>'l', 'stat'=>'s', 'oper'=>'o' ];
+
+function getname($flag)
+{
+  GLOBAL $base;
+
+  $name = $_GET['f'];
+  $pi = pathinfo($name);
+  if ($name != $pi['filename'] || $name=='')
+    die("wrong name (parameter $flag)");
+  return "$base/e/$name.tpl";
+}
+
+function renamer($name)
+{
+ if (!file_exists($name))
+   return 0;
+ for ($i=0;; $i++)
+   {
+     $to = "$name.tpl.~$i~";
+     if (!file_exists($to))
+       {
+         rename($name,$to);
+         return 1;
+       }
+   }
+}
+
+function fail($reason)
+{
+  header("HTTP/1.0 500 $reason");
+}
 
 $r = $_GET['r'];
 if (isset($dirs[$r]))
@@ -27,30 +58,27 @@ if (isset($dirs[$r]))
         echo "\n";
       }
   }
-elseif ($r=="save")
+elseif ($r=='kick')
   {
     $name = $_GET['f'];
     $pi = pathinfo($name);
     if ($name!=$pi['filename'] || $name=='')
-      die("wrong name (parameter f)");
-    $name = "$base/e/$name.tpl";
-    $done = "created";
-    if (file_exists($name))
-      for ($i=0;; $i++)
-        {
-          $to = "$name.tpl.~$i~";
-          if (!file_exists($to))
-            {
-              rename($name,$to);
-              $done = "replaced";
-              break;
-            }
-        }
+      die('wrong name (parameter f)');
+    $name	= getname('f');
+    if (renamer(getname('f')))
+      echo 'kicked';
+    else
+      fail('unknown');
+  }
+elseif ($r=='save')
+  {
+    $name	= getname('f');
+    $done	= renamer($name) ? 'renamed' : 'created';
     if (copy('php://input', $name))
       echo $done;
     else
-      header("HTTP:/1.0 500 copy failed");
+      fail('copy failed');
   }
 else
-  die("wrong param r");
+  die('wrong param r');
 
