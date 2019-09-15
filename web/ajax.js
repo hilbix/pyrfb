a826abedfa2c25d0b3277057033b1b7297d6a6bb
Valentin Hilbig <valentin.hilbig@sol-venetus.de>
2019-09-15 10:54:49 +0200

    web/ajax.js modified

--- web/ajax.js
+++ web/ajax.js
@@ -30,13 +30,14 @@ function _a_(href,onclick){var a=__.apply(this,Ary(['a'],0,arguments,2));a.href=
 ajax={};
 ajax.collect=function(a,f){var n=[];for(var i=0;i<a.length;i++){var v=f(a[i]);if(v!=null)n.push(v)}return n};
 ajax.x=function(){try{return new XMLHttpRequest()}catch(e){try{return new ActiveXObject('Msxml2.XMLHTTP')}catch(e){return new ActiveXObject('Microsoft.XMLHTTP')}}};
-ajax.send=function(u,f,m,a,h){var x=ajax.x();x.open(m,u,true);x.onreadystatechange=function(){if(x.readyState==4)f(x.responseText,x,x.status,x.getResponseHeader("Last-Modified"))};if(m=='POST')x.setRequestHeader('Content-type','application/x-www-form-urlencoded');if(h)h(x);x.send(a)};
+ajax.send=function(u,f,m,a,h){var x=ajax.x(),t='x-www-form-urlencoded';if(m=='POSTbin'){t='binary';m='POST'};x.open(m,u,true);x.onreadystatechange=function(){if(x.readyState==4)f(x.responseText,x,x.status,x.getResponseHeader("Last-Modified"))};if(m=='POST')x.setRequestHeader('Content-type','application/'+t);if(h)h(x);x.send(a)};
 ajax.get=function(url,func){ajax.send(url,func,'GET')};
 ajax.update=function(u,f,lm){ajax.send(u,f,'GET',null,lm?function(x){x.setRequestHeader("If-Modified-Since",lm)}:lm)};
 ajax.head=function(u,f,lm){ajax.send(u,f,'HEAD',null,lm?function(x){x.setRequestHeader("If-Modified-Since",lm)}:lm)};
 //ajax.serialize=function(f){var g=function(n){return f.getElementsByTagName(n)};var nv=function(e){if(e.name)return encodeURIComponent(e.name)+'='+encodeURIComponent(e.value);else return ''};var i=ajax.collect(g('input'),function(i){if((i.type!='radio'&&i.type!='checkbox')||i.checked)return nv(i)});var s=ajax.collect(g('select'),nv);var t=ajax.collect(g('textarea'),nv);return i.concat(s).concat(t).join('&');};
 //ajax.gets=function(url){var x=ajax.x();x.open('GET',url,false);x.send(null);return x.responseText};
 ajax.post=function(url,func,args){ajax.send(url,func,'POST',args)};
+ajax.push=function(url,func,args){ajax.send(url,func,'POSTbin',args)};
 //ajax.update=function(url,elm){var e=$(elm);var f=function(r){e.innerHTML=r};ajax.get(url,f)};
 //ajax.submit=function(url,elm,frm){var e=$(elm);var f=function(r){e.innerHTML=r};ajax.post(url,f,ajax.serialize(frm))};
 
