94407fd859810f3950f516b3c2d88d3932ba2db5
Valentin Hilbig <valentin.hilbig@sol-venetus.de>
2019-10-18 20:35:59 +0200

    php/macro.php modified

--- php/macro.php
+++ php/macro.php
@@ -3,23 +3,14 @@
 #
 # This Works is placed under the terms of the Copyright Less License,
 # see file COPYRIGHT.CLL.  USE AT OWN RISK, ABSOLUTELY NO WARRANTY.
-header("Content-type: text/plain");
-header("Pragma: no-cache");
-header("Expires: -1");
 
-$pi = explode('/', $_SERVER["PATH_INFO"]);
-$targ = intval($pi[1]);
+require 'sock.inc';
 
-$root = $_SERVER["DOCUMENT_ROOT"];
-$script = $_SERVER["SCRIPT_NAME"];      // this must be relative to document root
-$base = "$root/".dirname($script)."/$targ";
-if (!is_dir($base)) die("wrong $targ");
-
-$l = count($pi);
-if ($l!=3) die("too many macros: $l");
+plain();
+args(3,3);
 
 # There is only 1 .. for now
-for ($i=1; ++$i<$l; )
+for ($i=1; ++$i<$cnt; )
   {
     $f = $pi[$i];
     if ($f==='') die("missing filename: $i");
@@ -28,17 +19,9 @@ for ($i=1; ++$i<$l; )
   }
 
 # fetch arguments from POST data
-$r = file_get_contents("php://input");
+$r = get_POST_data();
 if (ctype_print($r))
   $t .= " $r";
 
-$fd = fsockopen("unix://../sub/$targ/sock");
-if (!$fd) die("cannot open $targ");
-socket_set_blocking($fd,1);
-fwrite($fd,"run$t\n");		# run (macro)
-fflush($fd);
-while (!feof($fd))
-  echo fread($fd,4096);
-fclose($fd);
-flush();
+send_receive("run$t");
 
